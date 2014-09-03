11faacc01ccd5220858d75a42bdb8e9b0c3389b8
Jonathan Hoyt <hoyt@github.com>
2014-09-02 18:51:46 -0700

    Build out search form UI and interaction

---
+++ client/components/SearchForm.js
@@ -0,0 +1,74 @@
+var React = require("react-atom-fork");
+var ReactPropTypes = React.PropTypes;
+
+var ENTER_KEY_CODE = 13;
+var element = null;
+
+function keyDownListener(event) {
+
+}
+
+var SearchForm = React.createClass({
+  displayName: "SearchForm",
+
+  propTypes: {
+    value: ReactPropTypes.string
+  },
+
+  getInitialState: function() {
+    return {
+      searchQuery: this.props.searchQuery || ''
+    };
+  },
+
+  componentDidMount: function() {
+    window.addEventListener("keydown", function(event) {
+      if(event.keyCode == 76 && event.metaKey){
+        document.getElementById("search").select();
+      }
+    });
+  },
+
+  render: function() {
+    var options = {
+      autoFocus: true,
+      className: "form-control search-box",
+      defaultValue: this.state.searchQuery,
+      id: "search",
+      key: "search",
+      onFocus: this._onFocus,
+      // onClick: this._onClick,
+      onChange: this._onChange,
+      onKeyDown: this._onKeyDown,
+      placeholder: "enter filename or search query",
+      ref: "searchBox"
+    }
+
+    return React.DOM.form({action: "#", className: "navbar-form search-form"},
+      React.DOM.input(options)
+    );
+  },
+
+  _onChange: function(event) {
+    this.setState({
+      searchQuery: event.target.value
+    });
+  },
+
+  _onKeyDown: function(event) {
+    if (event.keyCode === ENTER_KEY_CODE) {
+      event.preventDefault();
+      console.log("open or create file: " + event.target.value);
+    }
+  },
+
+  // _onClick: function(event) {
+  //   event.target.select();
+  // },
+
+  _onFocus: function(event) {
+    event.target.select();
+  }
+});
+
+module.exports = SearchForm;
